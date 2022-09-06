779c37d2d9535673fc8dcb17e366135067c264e9
João Witor Tenório <jwtcs@cin.ufpe.br>
2022-09-05 22:25:01 -0300

    inicializando a funcionalidade de tocar música

--- src/ArmazemSong.java
+++ src/ArmazemSong.java
@@ -12,7 +12,7 @@ public class ArmazemSong {
         songList.add(a);
     }
 
-    public String[][] setListaReproducao() {
+    public String[][] getListaReproducao() {
         String[][] listaReproducao = new String[songList.size()][6];
         for (int i = 0; i < songList.size(); i++) {
             listaReproducao[i][0] = songList.get(i).getTitle();
@@ -33,6 +33,11 @@ public class ArmazemSong {
         }
     }
 
+    public Song getSong(String id) {
+        int posicao = buscarPosicao(id);
+        return songList.get(posicao);
+    }
+
     public int buscarPosicao(String id) {
         for (int i = 0; i < songList.size(); i++) {
             if (songList.get(i).getUuid() == id) {
@@ -42,4 +47,7 @@ public class ArmazemSong {
         return -1;
     }
 
+    public ArrayList setSongsArmazenados(){
+        return  songList;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
 import javazoom.jl.decoder.*;
 import javazoom.jl.player.AudioDevice;
+import javazoom.jl.player.FactoryRegistry;
 import support.PlayerWindow;
 import support.Song;
 
@@ -9,6 +10,8 @@ import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Player {
@@ -31,9 +34,15 @@ public class Player {
     private int currentFrame = 0;
 
     private final ActionListener buttonListenerPlayNow = e -> {
-
+        try {
+            playNow();
+        } catch (JavaLayerException ex) {
+            throw new RuntimeException(ex);
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
     };
-    private final ActionListener buttonListenerRemove = e ->  {
+    private final ActionListener buttonListenerRemove = e -> {
         removerSong();
     };
     private final ActionListener buttonListenerAddSong = e -> {
@@ -51,11 +60,16 @@ public class Player {
     };
     private final ActionListener buttonListenerPlayPause = e -> {
     };
-    private final ActionListener buttonListenerStop = e ->{ };
-    private final ActionListener buttonListenerNext = e ->{ };
-    private final ActionListener buttonListenerPrevious = e ->{ };
-    private final ActionListener buttonListenerShuffle = e -> { };
-    private final ActionListener buttonListenerLoop = e -> { };
+    private final ActionListener buttonListenerStop = e -> {
+    };
+    private final ActionListener buttonListenerNext = e -> {
+    };
+    private final ActionListener buttonListenerPrevious = e -> {
+    };
+    private final ActionListener buttonListenerShuffle = e -> {
+    };
+    private final ActionListener buttonListenerLoop = e -> {
+    };
     private final MouseInputAdapter scrubberMouseInputAdapter = new MouseInputAdapter() {
         @Override
         public void mouseReleased(MouseEvent e) {
@@ -71,11 +85,12 @@ public class Player {
     };
 
     private ArmazemSong armSong;
+
     public Player() {
         this.armSong = new ArmazemSong();
         EventQueue.invokeLater(() -> this.window = new PlayerWindow(
                 "CinMusic",
-                this.armSong.setListaReproducao(),
+                this.armSong.getListaReproducao(),
                 buttonListenerPlayNow,
                 buttonListenerRemove,
                 buttonListenerAddSong,
@@ -138,13 +153,57 @@ public class Player {
     private void adicionarSong() throws InvalidDataException, UnsupportedTagException, IOException, BitstreamException {
         Song a = this.window.openFileChooser();
         this.armSong.addSongList(a);
-        this.window.setQueueList(this.armSong.setListaReproducao());
+        this.window.setQueueList(this.armSong.getListaReproducao());
     }
 
-    private void removerSong(){
+    private void removerSong() {
         String id = this.window.getSelectedSong();
         this.armSong.removeSongList(id);
-        this.window.setQueueList(this.armSong.setListaReproducao());
+        this.window.setQueueList(this.armSong.getListaReproducao());
+    }
+
+    private void playNow() throws JavaLayerException, FileNotFoundException {
+        Song atual = this.armSong.getSong(this.window.getSelectedSong());
+        play(atual);
+    }
+
+    private void play(Song a) throws FileNotFoundException {
+        BufferedInputStream bin = a.getBufferedInputStream();
+        new Thread(() -> {
+            try {
+                this.device = FactoryRegistry.systemRegistry().createAudioDevice();
+            } catch (JavaLayerException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                this.device.open(this.decoder = new Decoder());
+            } catch (JavaLayerException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                this.bitstream = new Bitstream(a.getBufferedInputStream());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            this.currentFrame = 0;
+
+            while (true){
+                try {
+                    if (!this.playNextFrame()) break;
+                } catch (JavaLayerException e) {
+                    throw new RuntimeException(e);
+                }
+                this.window.setPlayingSongInfo(a.getTitle(),a.getAlbum(),a.getArtist());
+            }
+
+
+        }).start();
     }
     //</editor-fold>
+
+    protected AudioDevice getAudioDevice() throws JavaLayerException {
+        return FactoryRegistry.systemRegistry().createAudioDevice();
+    }
 }
+
+
